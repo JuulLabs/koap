0db348eeb57082d6d5eeb5616965980272e5c81a
Travis Wyatt <travis@juul.com>
2020-06-13 20:39:33 -0700

    Add documentation re: token encoding (bytes occupied)

--- koap/src/main/kotlin/Message.kt
+++ koap/src/main/kotlin/Message.kt
@@ -42,7 +42,25 @@ private val SIZE1_RANGE = UINT_RANGE
 sealed class Message {
 
     abstract val code: Code
+
+    /**
+     * Per RFC 7252 2.2. Request/Response Model:
+     *
+     * > A Token is used to match responses to requests independently from the underlying messages.
+     *
+     * A token may be 0-8 bytes, whereas [token] is represented as a [Long]. The following ranges
+     * outline the number of bytes that will be occupied when encoded as CoAP.
+     *
+     * | Range                          | Bytes |
+     * |--------------------------------|-------|
+     * |         -2^63 .. -1            | 8     |
+     * |             0 .. 255           | 1     |
+     * |           256 .. 65,635        | 2     |
+     * |        65,536 .. 4,294,967,295 | 4     |
+     * | 4,294,967,296 .. 2^63-1        | 8     |
+     */
     abstract val token: Long?
+
     abstract val options: List<Option>
     abstract val payload: ByteArray
 
