bafba923cd7fc28daef396087f886ad7634d5b36
Travis Wyatt <travis@juul.com>
2020-10-27 12:09:31 -0700

    Clean up Gradle configuration

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,12 @@
+fun mockk(
+    version: String = "1.10.0"
+): String = "io.mockk:mockk:$version"
+
+fun equalsverifier(
+    version: String = "3.4"
+): String = "nl.jqno.equalsverifier:equalsverifier:$version"
+
+fun okio(
+    artifact: String,
+    version: String = "2.9.0"
+): String = "com.squareup.okio:$artifact:$version"
