7b1edd0954599138f017e7279dae08ad9bb3b64b
Justin Tiffner <justin.tiffner@juul.com>
2020-11-13 12:20:40 -0800

    Removing helper file as it's not needed.

--- koap/js_tests/helpers.js
+++
@@ -1,7 +0,0 @@
-global.byteToHexString = (byte) => {
-    return '' + ('0' + (byte & 0xFF).toString(16)).slice(-2)
-};
-  
-global.byteArrayToHexString = (byteArray, separator = ' ') => {
-    return Array.from(byteArray, byteToHexString).join(separator)
-};
