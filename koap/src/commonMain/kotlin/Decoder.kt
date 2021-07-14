f3bfb16f44a998a8275377bd79ba06e09b7a70cb
KVelasco <kevin.velasco@gmail.com>
2021-07-13 22:25:18 -0700

    Remove Js annotations (#53)
    * Remove Js annotations
    
    * Remove Js annotations

--- koap/src/commonMain/kotlin/Decoder.kt
+++ koap/src/commonMain/kotlin/Decoder.kt
@@ -48,8 +48,6 @@ import com.juul.koap.Message.Udp.Type.Confirmable
 import com.juul.koap.Message.Udp.Type.NonConfirmable
 import com.juul.koap.Message.Udp.Type.Reset
 import okio.BufferedSource
-import kotlin.js.JsExport
-import kotlin.js.JsName
 
 /**
  * Decodes [ByteArray] receiver to a [Message].
@@ -99,7 +97,6 @@ inline fun <reified T : Message> ByteArray.decode(): T =
  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
  * ```
  */
-@JsExport
 fun ByteArray.decodeUdp(): Message.Udp {
     val header = decodeUdpHeader()
     return decode(header, offset = header.size)
@@ -123,7 +120,6 @@ fun ByteArray.decodeUdp(): Message.Udp {
  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
  * ```
  */
-@JsExport
 fun ByteArray.decodeTcp(): Message.Tcp {
     val header = decodeTcpHeader()
     return decode(header, offset = header.size)
@@ -150,8 +146,6 @@ fun ByteArray.decodeTcp(): Message.Tcp {
  * val message = encoded.decode(content, offset = 0)
  * ```
  */
-@JsExport
-@JsName("decodeWithUdpHeader")
 fun ByteArray.decode(
     header: Header.Udp,
     offset: Int = header.size
@@ -178,8 +172,6 @@ fun ByteArray.decode(
  * val message = content.decode(content, offset = 0)
  * ```
  */
-@JsExport
-@JsName("decodeWithTcpHeader")
 fun ByteArray.decode(
     header: Header.Tcp,
     offset: Int = header.size
@@ -237,7 +229,6 @@ private fun ByteArray.decodeContent(
  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
  * ```
  */
-@JsExport
 fun ByteArray.decodeUdpHeader(): Header.Udp = withReader {
     // |7 6 5 4 3 2 1 0|
     // +-+-+-+-+-+-+-+-+
@@ -288,7 +279,6 @@ fun ByteArray.decodeUdpHeader(): Header.Udp = withReader {
  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
  * ```
  */
-@JsExport
 fun ByteArray.decodeTcpHeader(): Header.Tcp = withReader {
     // |7 6 5 4 3 2 1 0|
     // +-+-+-+-+-+-+-+-+
