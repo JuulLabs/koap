96d36c6dc6c4690afd124c76f4e0348219916f7c
Travis Wyatt <travis@juul.com>
2021-12-18 14:58:54 -0800

    Use node 16.13.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,10 @@ allprojects {
     }
 }
 
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = libs.versions.node.get()
+}
+
 task<Copy>("assembleGitHubPages") {
     description = "Generates static web site for GitHub Pages."
     group = "Build"
