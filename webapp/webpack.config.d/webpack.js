5ef427b6dbde087f20d7b3c6bb37011bb5ec22a7
Travis Wyatt <travis@juul.com>
2021-12-20 23:42:53 -0800

    Additional webpack polyfills

--- webapp/webpack.config.d/webpack.js
+++ webapp/webpack.config.d/webpack.js
@@ -2,5 +2,7 @@ config.resolve.fallback = {
     "stream": require.resolve("stream-browserify"),
     "util": require.resolve("util/"),
     "buffer": require.resolve("buffer/"),
-    "os": require.resolve("os-browserify/browser")
+    "os": require.resolve("os-browserify/browser"),
+    "process": require.resolve("process/"),
+    "path": require.resolve("path-browserify")
 }
