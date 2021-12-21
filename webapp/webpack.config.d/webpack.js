492ebd5439b63aa11a2bfb95b3f20cceac2f0915
Travis Wyatt <travis@juul.com>
2021-12-20 23:50:31 -0800

    Fix `process` polyfill missing

--- webapp/webpack.config.d/webpack.js
+++ webapp/webpack.config.d/webpack.js
@@ -1,8 +1,16 @@
+const webpack = require('webpack');
+
+config.plugins = [
+    new webpack.ProvidePlugin({
+        process: 'process/browser',
+    })
+]
+
 config.resolve.fallback = {
     "stream": require.resolve("stream-browserify"),
     "util": require.resolve("util/"),
     "buffer": require.resolve("buffer/"),
     "os": require.resolve("os-browserify/browser"),
-    "process": require.resolve("process/"),
+    "process": require.resolve("process/browser"),
     "path": require.resolve("path-browserify")
 }
