3487cd2b2a2c902d9ae2acdbcaaf82d9bb29d669
Travis Wyatt <travis@juul.com>
2023-02-23 09:39:09 -0800

    Remove metadata workaround (#170)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,6 @@ buildscript {
     repositories {
         mavenCentral()
     }
-
-    dependencies {
-        // Workaround for:
-        // > Incompatible version of Kotlin metadata.
-        // > Maximal supported Kotlin metadata version: 1.5.1,
-        // > com/juul/koap/ByteArrayReader Kotlin metadata version: 1.7.1.
-        // > As a workaround, it is possible to manually update 'kotlinx-metadata-jvm' version in your project.
-        //
-        // todo: Remove when binary-compatibility-validator bundles support for metadata 1.7.x.
-        classpath("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.6.0")
-    }
 }
 
 plugins {
