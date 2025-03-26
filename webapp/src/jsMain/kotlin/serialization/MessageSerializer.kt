5198048e695505ea5423e27470c04f4b5595b1af
Mattias de Zalenski <zalenski@northvolt.com>
2025-03-26 02:06:37 +0100

    Serialize more options like HTTP-headers

--- webapp/src/jsMain/kotlin/serialization/MessageSerializer.kt
+++ webapp/src/jsMain/kotlin/serialization/MessageSerializer.kt
@@ -133,6 +133,7 @@ private object OptionSerializer : KSerializer<Option> {
 
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Option", PrimitiveKind.STRING)
 
+    @ExperimentalStdlibApi // for `toHexString`
     override fun serialize(encoder: Encoder, value: Option) {
         fun format(format: Long): String = when (format) {
             PlainText.format -> "text/plain; charset=utf-8"
@@ -145,8 +146,25 @@ private object OptionSerializer : KSerializer<Option> {
             else -> format.toString()
         }
         val serialized = when (value) {
+            is Option.IfMatch -> "If-Match: " + value.etag.toHexString()
+            is Option.UriHost -> "Uri-Host: " + value.uri
+            is Option.ETag -> "ETag: " + value.etag.toHexString()
+            is Option.IfNoneMatch -> "If-None-Match"
+            is Option.UriPort -> "Uri-Port: " + value.port
+            is Option.LocationPath -> "Location-Path: " + value.uri
+            is Option.UriPath -> "Uri-Path: " + value.uri
             is Option.ContentFormat -> "Content-Format: " + format(value.format)
+            is Option.MaxAge -> "Max-Age: " + value.seconds
+            is Option.UriQuery -> "Uri-Query: " + value.uri
+            is Option.HopLimit -> "Hop-Limit: " + value.hops
             is Option.Accept -> "Accept: " + format(value.format)
+            is Option.LocationQuery -> "Location-Query: " + value.uri
+            is Option.ProxyUri -> "Proxy-Uri: " + value.uri
+            is Option.ProxyScheme -> "Proxy-Scheme: " + value.uri
+            is Option.Size1 -> "Size1: " + value.bytes
+            is Option.Echo -> "Echo: " + value.value.toHexString()
+            is Option.NoResponse -> "No-Response: " + value.value
+            is Option.RequestTag -> "Request-Tag: " + value.tag.toHexString()
             else -> value.toString()
         }
         encoder.encodeString(serialized)
