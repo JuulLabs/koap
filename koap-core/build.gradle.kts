9c3982db5379451f8e4daa3e026d53fb2d1a4e41
Cedrick Cooke <development@cedrickc.net>
2026-01-15 10:11:24 -0800

    Simplify test dependencies

--- koap-core/build.gradle.kts
+++ koap-core/build.gradle.kts
@@ -25,21 +25,13 @@ kotlin {
 
         val commonTest by getting {
             dependencies {
-                implementation(kotlin("test-annotations-common"))
-                implementation(kotlin("test-common"))
+                implementation(kotlin("test"))
             }
         }
 
         val jvmTest by getting {
             dependencies {
-                implementation(kotlin("test-junit"))
                 implementation(libs.equalsverifier)
-            }
-        }
-
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
             }
         }
     }
