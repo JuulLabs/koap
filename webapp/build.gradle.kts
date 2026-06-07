fa70c55b8e6ee0dd9bddc95fdd8ffdfbd840d341
Travis Wyatt <travis@juul.com>
2026-06-07 15:07:13 -0700

    Fix build

--- webapp/build.gradle.kts
+++ webapp/build.gradle.kts
@@ -14,23 +14,19 @@ kotlin {
             languageSettings.optIn("kotlin.ExperimentalStdlibApi")
         }
 
-        val jsMain by getting {
-            dependencies {
-                implementation(project(":koap-core"))
-                implementation(libs.kotlinx.coroutines.js)
-                implementation(libs.kotlinx.serialization.json)
-                implementation(libs.okio.js)
-                implementation(npm("buffer", "6.0.3"))
-                implementation(npm("cbor", "8.1.0"))
-                implementation(npm("process", "0.11.10"))
-                implementation(npm("stream-browserify", "3.0.0"))
-            }
+        jsMain.dependencies {
+            implementation(project(":koap-core"))
+            implementation(libs.kotlinx.coroutines.js)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.okio.js)
+            implementation(npm("buffer", "6.0.3"))
+            implementation(npm("cbor", "8.1.0"))
+            implementation(npm("process", "0.11.10"))
+            implementation(npm("stream-browserify", "3.0.0"))
         }
 
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
-            }
+        jsTest.dependencies {
+            implementation(kotlin("test-js"))
         }
     }
 }
